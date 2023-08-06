c8cf15ba0727fd2947ca64a2e48094c5659d3cbb
Ammar <ammar.abdelnabi00@eng-st.cu.edu.eg>
2023-08-06 18:35:57 +0300

    delay list added

--- main_sep.py
+++ main_sep.py
@@ -19,8 +19,10 @@ diagram = mrsd.Diagram(
 
 ############################### Time sorting ##########################
 
-def middle(n):  
-    return n[1] 
+
+def middle(n):
+    return n[1]
+
 
 data = params
 RF = params['RF']
@@ -29,35 +31,67 @@ MGR = data['Multi_gradient']
 TE = data['TE']
 TR = data['TR']
 
-TimeBaseList=[("TE",TE),("TR",TR)]
+TimeBaseList = [("TE",-1 ,TE), ("TR",-2, TR)]
 
 for item in RF:
     Time = item.get('Time')
-    #FA_Sign = item.get("Sign")+item.get("FA")
+    # FA_Sign = item.get("Sign")+item.get("FA")
     Duration = item.get('Duration')
     FA = item.get("FA")
     Sign = item.get("Sign")
-    TimeBaseList.append(("RF",Time,Duration,FA,Sign))
+    TimeBaseList.append(("RF", Time, Duration, FA, Sign))
 for item in Gr:
     Time = item.get('Time')
     Duration = item.get('Duration')
     Amp = item.get("Amp")
     Axis = item.get('Axis')
     Sign = item.get("Sign")
-    TimeBaseList.append(("Gr",Time,Duration,Amp,Axis,Sign))
+    TimeBaseList.append(("Gr", Time, Duration, Amp, Axis, Sign))
 for item in MGR:
     Time = item.get('Time')
     Amp = item.get("Amp")
     Axis = item.get('Axis')
     Sign = item.get("Sign")
-    TimeBaseList.append(("MGR",Time,Amp,Axis,Sign))
-    
-#TimeBaseList.sort(reverse=False,key=middle)
-print(TimeBaseList)
+    TimeBaseList.append(("MGR", Time, Amp, Axis, Sign))
+
+TimeBaseList.sort(reverse=False,key=middle)
+#print("TimeBaseList: ",TimeBaseList)
+
+############################### delay function ##########################
+
+delayList = []
+
+for i, item in enumerate(TimeBaseList):
+    if i < 3:
+        continue
+    if i < len(TimeBaseList)-1 and TimeBaseList[i][1] != TimeBaseList[i+1][1]:
+        delayList.append(("delay", TimeBaseList[i+1][1]-TimeBaseList[i][1]))
+    #print(i, item)
+#print("delayList: ",delayList)
+
+############################### delay appending ##########################
+MergedList=[TimeBaseList[0:2]]
+#print ("MergedList: ",MergedList )
+z=0
+for i, item in enumerate(TimeBaseList):
+    if i < 2:
+        continue
+    comparableIndex=i < len(TimeBaseList)-1
+    if i < len(TimeBaseList)-1 and z<len(delayList) and TimeBaseList[i+1][1]-TimeBaseList[i][1]==delayList[z][1]:
+        #delayList.append(("delay", TimeBaseList[i+1][1]-TimeBaseList[i][1]))
+        MergedList.extend([TimeBaseList[i],delayList[z]])
+        z=z+1
+    else:
+        MergedList.append(TimeBaseList[i])
+
+print("MergedList after:",MergedList )
+
+
+
+############################### write file ##########################
 
 with open('Time_file.txt', 'w') as f:
     f.writelines(str(TimeBaseList))
-
 
 if (1 == True):
     ################## Readout/Signal #################
@@ -70,11 +104,12 @@ if (1 == True):
     d_readout = Readout.get('d_readout')
     d_encoding = Readout.get('d_encoding')
     area_factor = Readout.get('area_factor')
-    #Duration=Readout.get("Duration")
+    # Duration=Readout.get("Duration")
     adc, echo, readout = diagram.readout(
-        "RO", "$G_x$", d_readout, ramp = d_ramp, center=TE,
+        "RO", "$G_x$", d_readout, ramp=d_ramp, center=TE,
         adc_kwargs={"ec": "0.5"})
-    diagram.add("$G_x$", readout.adapt(d_encoding, -0.5, d_ramp, end=readout.begin))
+    diagram.add("$G_x$", readout.adapt(
+        d_encoding, -0.5, d_ramp, end=readout.begin))
 
     ################### RF ###########################
 
@@ -84,11 +119,14 @@ if (1 == True):
         Time = item.get('Time')
         Sign = item.get('Sign')
         Duration = item.get('Duration')
-        if Sign =="-":
-            excitation, slice_selection = diagram.selective_pulse("RF", "$G_z$", duration=Duration,gradient_amplitude=-1, pulse_amplitude=Flip_A, ramp=d_ramp, center=Time)
-        elif Sign =="+":
-            excitation, slice_selection = diagram.selective_pulse("RF", "$G_z$", duration=Duration,gradient_amplitude=-1, pulse_amplitude=Flip_A, ramp=d_ramp, center=Time)
-        diagram.annotate("RF",excitation.center+0.5,2, r"$\alpha$ = " + str(Sign)+str(FA) )
+        if Sign == "-":
+            excitation, slice_selection = diagram.selective_pulse(
+                "RF", "$G_z$", duration=Duration, gradient_amplitude=-1, pulse_amplitude=Flip_A, ramp=d_ramp, center=Time)
+        elif Sign == "+":
+            excitation, slice_selection = diagram.selective_pulse(
+                "RF", "$G_z$", duration=Duration, gradient_amplitude=-1, pulse_amplitude=Flip_A, ramp=d_ramp, center=Time)
+        diagram.annotate("RF", excitation.center+0.5, 2,
+                         r"$\alpha$ = " + str(Sign)+str(FA))
     ################## Gradient ####################
     Gradient = Gr
 
@@ -101,7 +139,7 @@ if (1 == True):
         elif (item.get('Axis') == "x"):
             diagram.gradient("$G_x$", Duration, Amp, center=Time)
         elif (item.get('Axis') == "z"):
-            diagram.gradient("$G_z$", Amp,0.8,ramp=0.1, center=Time)
+            diagram.gradient("$G_z$", Amp, 0.8, ramp=0.1, center=Time)
 
     ################# Multi Gradient #############
     Multi_gradient = MGR
@@ -111,23 +149,32 @@ if (1 == True):
         Amp = item.get('Amp')
         Sign = item.get('Sign')
         if (item.get('Axis') == "y"):
-            multiGradient = diagram.multi_gradient("$G_y$", d_encoding, 0.5, d_ramp, center = Time)
-            if Sign=="+":
-                diagram.annotate("$G_y$",multiGradient.center+0.5, 0, r"$\uparrow$" )
-            elif Sign=="-":
-                diagram.annotate("$G_y$",multiGradient.center+0.5, 0, r"$\downarrow$" )
+            multiGradient = diagram.multi_gradient(
+                "$G_y$", d_encoding, 0.5, d_ramp, center=Time)
+            if Sign == "+":
+                diagram.annotate(
+                    "$G_y$", multiGradient.center+0.5, 0, r"$\uparrow$")
+            elif Sign == "-":
+                diagram.annotate(
+                    "$G_y$", multiGradient.center+0.5, 0, r"$\downarrow$")
         elif (item.get('Axis') == "x"):
-            multiGradient = diagram.multi_gradient("$G_x$", d_encoding, 0.5, d_ramp, center = Time)
-            if Sign=="+":
-                diagram.annotate("$G_x$",multiGradient.center+0.5, 0, r"$\uparrow$" )
-            elif Sign=="-":
-                diagram.annotate("$G_x$",multiGradient.center+0.5, 0, r"$\downarrow$" )
+            multiGradient = diagram.multi_gradient(
+                "$G_x$", d_encoding, 0.5, d_ramp, center=Time)
+            if Sign == "+":
+                diagram.annotate(
+                    "$G_x$", multiGradient.center+0.5, 0, r"$\uparrow$")
+            elif Sign == "-":
+                diagram.annotate(
+                    "$G_x$", multiGradient.center+0.5, 0, r"$\downarrow$")
         elif (item.get('Axis') == "z"):
-            multiGradient = diagram.multi_gradient("$G_z$", d_encoding, 0.5, d_ramp, center = Time)
-            if Sign=="+":
-                diagram.annotate("$G_z$",multiGradient.center+0.5, 0, r"$\uparrow$" )
-            elif Sign=="-":
-                diagram.annotate("$G_z$",multiGradient.center+0.5, 0, r"$\downarrow$" )
+            multiGradient = diagram.multi_gradient(
+                "$G_z$", d_encoding, 0.5, d_ramp, center=Time)
+            if Sign == "+":
+                diagram.annotate(
+                    "$G_z$", multiGradient.center+0.5, 0, r"$\uparrow$")
+            elif Sign == "-":
+                diagram.annotate(
+                    "$G_z$", multiGradient.center+0.5, 0, r"$\downarrow$")
 
 
 ################## Annotations ###############
